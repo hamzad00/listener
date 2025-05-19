f2b37056f62bf9fdd4a90ff53b7baefdded0de89
hamzad00 <hamzad00@yahoo.com>
2025-05-19 18:25:17 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import time
 import threading
 import requests
@@ -10,13 +11,13 @@ app = Flask(__name__)
 def health():
     return {"status": "Listener is running"}, 200
 
-# کوکی‌ها
-SESSIONID = "5813075928%3ArncCPvoeRflkXL63A4lK%3A3AiAYcHk"
-DS_USER_ID = "5813075928"
-CSRFTOKEN = "NmIGD2ArXvRCKk48DdRg8"
-WEBHOOK_URL = "https://install.liara.run/webhook"
+# گرفتن متغیرهای محیطی
+SESSIONID = os.getenv("IG_SESSIONID")
+DS_USER_ID = os.getenv("IG_USERID")
+CSRFTOKEN = os.getenv("IG_CSRFTOKEN")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://install.liara.run/webhook")
 
-user_id = DS_USER_ID  # مستقیم از مقدار ثابت
+user_id = DS_USER_ID
 
 cl = Client()
 cl.set_settings({})
@@ -39,7 +40,7 @@ def check_messages():
                     }
                     try:
                         r = requests.post(WEBHOOK_URL, json=payload)
-                        print(f"[+] Sent to webhook {payload} | Status: {r.status_code}")
+                        print(f"[*] Sent to webhook {payload} | Status: {r.status_code}")
                     except Exception as e:
                         print(f"[!] Webhook error: {e}")
     last_checked = time.time()
@@ -49,6 +50,5 @@ def loop():
         check_messages()
         time.sleep(10)
 
-from threading import Thread
-Thread(target=loop).start()
+threading.Thread(target=loop).start()
 app.run(host="0.0.0.0", port=5000)
